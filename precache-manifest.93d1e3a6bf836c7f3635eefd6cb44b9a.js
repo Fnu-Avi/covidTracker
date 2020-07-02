cffa3caac0817d1e3fc9b93aeb8d0defe8e87dd1
Fnu Avi <Fnu.Avi@med.usc.edu>
2020-07-02 15:32:25 -0700

    Updates

---
+++ precache-manifest.93d1e3a6bf836c7f3635eefd6cb44b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4456de8fd42e6e55fd7d26de4faddae1",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "1a3acf6dfc44f6245192",
+    "url": "/covidTracker/static/css/main.36c95abf.chunk.css"
+  },
+  {
+    "revision": "60be0f53c808b8a9ac6a",
+    "url": "/covidTracker/static/js/2.7e823609.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.7e823609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3acf6dfc44f6245192",
+    "url": "/covidTracker/static/js/main.68e801a6.chunk.js"
+  },
+  {
+    "revision": "f04f4299982413a55661",
+    "url": "/covidTracker/static/js/runtime-main.f0c9259b.js"
+  }
+]);
